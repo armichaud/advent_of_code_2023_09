62d86591070475cc684d6cbd1623fc7ae1de1dac
armichaud <email@alexandermichaud.tech>
2023-12-09 09:10:41 -0500

    Solved part 2

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,42 @@ fn not_terminal(last_sequence: &Vec<i64>) -> bool{
     last_sequence.len() > 1 && !last_sequence.iter().all(|n| *n == 0)
 }
 
-fn solution_1(filename: &str) -> i64 { 
+fn build_sequences(history: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut sequences = Vec::new();
+    sequences.push(history.clone());
+    while not_terminal(sequences.last().unwrap()) {
+        // get diffs between each two adjacent members of last sequence
+        let mut diffs: Vec<i64> = Vec::new();
+        let last = sequences.last().unwrap();
+        for i in 0..last.len() - 1 {
+            diffs.push(last[i+1] - last[i]);
+        }
+        sequences.push(diffs);
+    }
+    sequences
+}
+
+fn solution(filename: &str, backwards: bool) -> i64 { 
     let histories = get_contents(filename);
     let mut sum = 0;
-
     for history in histories {
-        let mut sequences: Vec<Vec<i64>> = Vec::new();
-        sequences.push(history.clone());
-        while not_terminal(sequences.last().unwrap()) {
-            // get diffs between each two adjacent members of last sequence
-            let mut diffs: Vec<i64> = Vec::new();
-            let last = sequences.last().unwrap();
-            for i in 0..last.len() - 1 {
-                diffs.push(last[i+1] - last[i]);
-            }
-            sequences.push(diffs);
-        }
+        let sequences: Vec<Vec<i64>> = build_sequences(history);
         let mut k = 0;
         for seq in sequences.iter().rev() {
-            k += seq.last().unwrap();
+            if backwards {
+                k = seq.first().unwrap() - k;
+            } else {
+                k += seq.last().unwrap();
+            }
         }
         sum += k;
     }
-
     sum
 }
 
 fn main() {
-    assert_eq!(solution_1("example.txt"), 114);
-    assert_eq!(solution_1("input.txt"), 1938800261);
-    // assert_eq!(solution_2("example.txt"), 0);
-    // assert_eq!(solution_2("input.txt"), 0);
+    assert_eq!(solution("example.txt", false), 114);
+    assert_eq!(solution("input.txt", false), 1938800261);
+    assert_eq!(solution("example.txt", true), 2);
+    assert_eq!(solution("input.txt", true), 1112);
 }
